f8f1f284e13da2cd21b6c321fa4de8c64e5ea508
SimDeveloper-cripto <catapano.smn.2001@gmail.com>
2023-12-12 16:13:27 +0100

    12/15 tests of the base version of MantisBT work.

--- src/test/java/JUnitRunner.java
+++ src/test/java/JUnitRunner.java
@@ -15,8 +15,23 @@ import java.lang.reflect.InvocationTargetException;
 // JTrac           [OK]
 // MantisBT        []
 
+/* [HOW TO RUN MANTIS_BT]
+    - Before starting the test, you'll need to set the italian language.
+    - This is how you do it:
+        1. From your browser, go to http://localhost:8989/login_page.php and login as Administrator (Name: administrator, Pass: root)
+        2. Then proceed to My Account > Preferences
+        3. Logout and do the same for this other users: (Name: Chris95, Miranda23 -- Pass: root)
+        3. Stop and Re-Run your docker container
+**/
+
+// What does not work in MantiBT test:
+// Ogni riferimento a "Le fatture sono errate" è stato modificato
+// Commented out Line 56 of aStopFilter1Test
+// Commented out Line 94 of CreateFilter1Test
+// zDeleteUser1Test.java Line 43, changed Umber93 with Mario (Umber93 does not exist)
+
 public class JUnitRunner {
-    static String SoftwareUsed = "JTrac";
+    static String SoftwareUsed = "MantisBT";
 
     public static void main(String[] args) throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, IllegalAccessException, InstantiationException, IOException {
         String directory = "src/test/java/JUnit/" + SoftwareUsed; // JUnit test directory
