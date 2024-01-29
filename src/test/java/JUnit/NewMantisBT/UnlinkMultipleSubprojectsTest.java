ddc0ba18502f4415f544727421708f0c5f39c394
SimDeveloper-cripto <catapano.smn.2001@gmail.com>
2024-01-29 15:00:14 +0100

    The new mantisbt tests have a problem

--- src/test/java/JUnit/NewMantisBT/UnlinkMultipleSubprojectsTest.java
+++ src/test/java/JUnit/NewMantisBT/UnlinkMultipleSubprojectsTest.java
@@ -1,19 +1,12 @@
 package JUnit.NewMantisBT;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
 
-import java.util.concurrent.TimeUnit;
-
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class UnlinkMultipleSubprojectsTest {
 
@@ -29,24 +22,30 @@ public class UnlinkMultipleSubprojectsTest {
 	@Test
 	public void unlinkMultipleSubprojects() throws Exception {
 		driver.get("http://localhost:8080/login_page.php");
+
 		driver.findElement(By.name("username")).clear();
 		driver.findElement(By.name("username")).sendKeys("administrator");
 		driver.findElement(By.name("password")).clear();
 		driver.findElement(By.name("password")).sendKeys("root");
+
 		driver.findElement(By.cssSelector("input.button")).click();
 		driver.findElement(By.linkText("Manage")).click();
 		driver.findElement(By.linkText("Manage Projects")).click();
 		driver.findElement(By.linkText("Project002")).click();
+
 		driver.findElement(By.xpath("html/body/div[5]/table/tbody/tr[3]/td[7]/span[2]/a")).click();
 		driver.findElement(By.linkText("Proceed")).click();
 		driver.findElement(By.xpath("html/body/div[5]/table/tbody/tr[3]/td[7]/span[2]/a")).click();
 		driver.findElement(By.linkText("Proceed")).click();
 		driver.navigate().refresh();
+
 		assertFalse(isElementPresent(By.xpath("html/body/div[6]/table/tbody/tr[3]/td[1]/a")));
 		assertFalse(isElementPresent(By.xpath("html/body/div[6]/table/tbody/tr[4]/td[1]/a")));
 		driver.findElement(By.linkText("Manage Projects")).click();
+
 		assertEquals("sub12", driver.findElement(By.xpath("html/body/table[3]/tbody/tr[4]/td[1]/a")).getText());
 		assertEquals("sub22", driver.findElement(By.xpath("html/body/table[3]/tbody/tr[5]/td[1]/a")).getText());
+
 		driver.findElement(By.linkText("Logout")).click();
 	}
 
